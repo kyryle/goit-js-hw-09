774612261c1cddf0719df0c496ea2e7f2b66e7a5
user <kyryle0110@gmail.com>
2026-06-11 20:57:57 +0300

    done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,13 +6,16 @@ inputListiner.addEventListener("input", event => {
         message: DataOfForm.get("message"),
     }
         saveToLS("userData", data)
-    document.addEventListener("DOMContentLoaded", (e) => {
-        const data = loadFromLS(key)
-        inputListiner.elements.email.value = data.email
-        inputListiner.elements.message.value = data.message
-    });
    
 })
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    const data = loadFromLS("userData")
+    if (data) {
+        inputListiner.elements.email.value = data.email
+        inputListiner.elements.message.value = data.message
+    }
+    });
 
  function saveToLS(key, value) {
         const json = JSON.stringify(value)
@@ -25,9 +28,22 @@ inputListiner.addEventListener("input", event => {
         } catch {
             return json
         }
-    }
+}
+    
+
 
 inputListiner.addEventListener("submit", event => {
+    const DataOfForm = new FormData(inputListiner)
+    const data = {
+        email: DataOfForm.get("email"),
+        message: DataOfForm.get("message"),
+    }
+    
     event.preventDefault()
+    if (inputListiner.elements.email.value.trim().length === 0 || inputListiner.elements.message.value.trim().length === 0) {
+        return alert("fill in email and message")
+    }
     inputListiner.reset()
+    console.log(data)
+    localStorage.removeItem("userData")
 })
