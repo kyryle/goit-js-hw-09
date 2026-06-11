3045a63dfcc718667349097eac5198dc10e05906
user <kyryle0110@gmail.com>
2026-06-11 21:15:15 +0300

    done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,10 +1,11 @@
 const inputListiner = document.querySelector("form.feedback-form")
 inputListiner.addEventListener("input", event => {
-    const DataOfForm = new FormData(inputListiner)
-    const data = {
-        email: DataOfForm.get("email"),
-        message: DataOfForm.get("message"),
-    }
+    let DataOfForm = new FormData(inputListiner)
+    const data = Object.fromEntries(DataOfForm.entries());
+    // const data = {
+    //     email: DataOfForm.get("email"),
+    //     message: DataOfForm.get("message"),
+    // }
         saveToLS("userData", data)
    
 })
@@ -33,12 +34,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 
 inputListiner.addEventListener("submit", event => {
-    const DataOfForm = new FormData(inputListiner)
-    const data = {
-        email: DataOfForm.get("email"),
-        message: DataOfForm.get("message"),
-    }
-    
+    let DataOfForm = new FormData(inputListiner)
+    const data = Object.fromEntries(DataOfForm.entries());
+
     event.preventDefault()
     if (inputListiner.elements.email.value.trim().length === 0 || inputListiner.elements.message.value.trim().length === 0) {
         return alert("fill in email and message")
@@ -46,4 +44,6 @@ inputListiner.addEventListener("submit", event => {
     inputListiner.reset()
     console.log(data)
     localStorage.removeItem("userData")
+    DataOfForm = {}
+    console.log(DataOfForm)
 })
