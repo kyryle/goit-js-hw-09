b0d01f768b7b5f858b702a38a314b0a4a2a27ed2
user <kyryle0110@gmail.com>
2026-06-11 17:20:33 +0300

    task 2 problem check

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,13 @@
-const fillerCode = {}
+const inputListiner = document.querySelector("form")
+inputListiner.addEventListener("input", event => {
+    let emailValue = inputListiner.email.value
+    let messageValue = inputListiner.message.value
+    let emailSave = localStorage.setItem("Email", emailValue)
+    let messageSave = localStorage.setItem("Message", messageValue)
+    console.log(emailSave)
+    document.addEventListener("DOMContentLoaded", (e) => {
+        inputListiner.email.value = emailSave
+        inputListiner.message.value = messageSave
+});
+})
+
