8bbd4b698ce0d38b301dfbd44d0c5e45dbe89a2b
user <kyryle0110@gmail.com>
2026-06-11 20:34:05 +0300

    check

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,13 +1,33 @@
-const inputListiner = document.querySelector("form")
+const inputListiner = document.querySelector("form.feedback-form")
 inputListiner.addEventListener("input", event => {
-    let emailValue = inputListiner.email.value
-    let messageValue = inputListiner.message.value
-    let emailSave = localStorage.setItem("Email", emailValue)
-    let messageSave = localStorage.setItem("Message", messageValue)
-    console.log(emailSave)
+    const DataOfForm = new FormData(inputListiner)
+    const data = {
+        email: DataOfForm.get("email"),
+        message: DataOfForm.get("message"),
+    }
+        saveToLS("userData", data)
     document.addEventListener("DOMContentLoaded", (e) => {
-        inputListiner.email.value = emailSave
-        inputListiner.message.value = messageSave
-});
+        const data = loadFromLS(key)
+        inputListiner.elements.email.value = data.email
+        inputListiner.elements.message.value = data.message
+    });
+   
 })
 
+ function saveToLS(key, value) {
+        const json = JSON.stringify(value)
+        localStorage.setItem(key, json)
+    }
+    function loadFromLS(key) {
+        const json = localStorage.getItem(key)
+        try {
+            return JSON.parse(json)
+        } catch {
+            return json
+        }
+    }
+
+inputListiner.addEventListener("submit", event => {
+    event.preventDefault()
+    inputListiner.reset()
+})
